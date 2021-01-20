14717426ff7fa8d0766c9c35b251e5b06669074f
abbarco <68168827+abbarco@users.noreply.github.com>
2021-01-19 19:28:29 -0500

    Test clase VideoGameRental

--- src/test/java/com/espol/rentalsystemtest/CustomerTest.java
+++ src/test/java/com/espol/rentalsystemtest/CustomerTest.java
@@ -5,73 +5,48 @@
  */
 package com.espol.rentalsystemtest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author edwin
- */
-public class CustomerTest {
-    
-    public CustomerTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VideoGameRentalTest{
+	Ps3Game littleBigPlanet;
+    Xbox360Game fable2;
+    WiiGame superSmashBrosBrawl;
+	
+	@BeforeEach
     public void setUp() {
+    	littleBigPlanet = new Ps3Game("Little Big Planet");
+    	fable2 = new Xbox360Game("Fable 2");
+    	superSmashBrosBrawl = new WiiGame("Super Smash Bros. Brawl");   
     }
     
-    @AfterEach
-    public void tearDown() {
-    }
-
     @Test
-    public void testAddMovieRental() {
-        System.out.println("addMovieRental");
-        MovieRental arg = new MovieRental(new Movie("Avengers Endgame",30),15);
-        Customer instance = new Customer("Victor Aguilera Lascano");
-        instance.addMovieRental(arg);
-        assertEquals(arg, instance.getMovieRentals().get(instance.getMovieRentals().size()-1));
+    public void testStatementPs3GameOnly() {
+        // Ps3 games cost $4.00 for the first 4 days, and $1.250/day thereafter
+        // a rental earns 2 frequent-renter point no matter how many days
+        Customer johnDoe = new Customer("John Doe");
+        johnDoe.addVideoGameRental(new VideoGameRental(littleBigPlanet, 1, false));
+        assertEquals("Rental Record for John Doe\n" +
+                "\tLittle Big Planet\t4.0\n" +
+                "Amount owed is 4.0\n" +
+                "You earned 2 frequent renter points",
+           johnDoe.statement());
+        johnDoe.addVideoGameRental(new VideoGameRental(littleBigPlanet, 3, false));
+        assertEquals("Rental Record for John Doe\n" +
+                "\tLittle Big Planet\t4.0\n" +
+                "\tLittle Big Planet\t4.0\n" +
+                "Amount owed is 8.0\n" +
+                "You earned 4 frequent renter points",
+           johnDoe.statement());
+        johnDoe.addVideoGameRental(new VideoGameRental(littleBigPlanet, 5, false));
+        assertEquals("Rental Record for John Doe\n" +
+                "\tLittle Big Planet\t4.0\n" +
+                "\tLittle Big Planet\t4.0\n" +
+                "\tLittle Big Planet\t5.25\n" +
+                "Amount owed is 13.25\n" +
+                "You earned 6 frequent renter points",
+           johnDoe.statement());
     }
-
-    /**
-     * Test of addVideoGameRental method, of class Customer.
-     */
-    
-    @Test
-    public void testAddVideoGameRental() {
-        System.out.println("addVideoGameRental");
-        VideoGameRental arg = new VideoGameRental("GTA San Andreas",15,true);
-        Customer instance = new Customer("Edwin");
-        instance.addVideoGameRental(arg);
-        assertEquals(arg, instance.getVideoRental().get(instance.getVideoRental().size()-1));
-    }
-
-
-    /**
-     * Test of statement method, of class Customer.
-     */
-    @Test
-    public void testStatement() {
-        System.out.println("statement");
-        Customer instance = null;
-        String expResult = "";
-        String result = instance.statement();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
